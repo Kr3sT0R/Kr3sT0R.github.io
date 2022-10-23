0477d0bc4d7e72d8bd43fe4cd2a36e48f2b9d9b0
Kr3sT0R <116526923+Kr3sT0R@users.noreply.github.com>
2022-10-24 02:45:36 +0300

    Add files via upload

---
+++ App/Scripts/js-script.js
@@ -0,0 +1,15 @@
+function clickButton() {
+	alert('Начинаем исследование!')
+}
+
+function clickButton1() {
+	alert('Узнаем их поближе?')
+}
+
+function clickButton2() {
+	alert('Что ещё интересного?')
+}
+
+function clickButton3() {
+	alert('Возвращаемся обратно!')
+}
